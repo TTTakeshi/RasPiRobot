be216d9f59f6ce31b3a0b56863066c141b92fdde
剛史 瀧口 <Takeshi.Takiguchi0403@outlook.com>
2018-12-29 21:28:23 +0900

    port3→29へ

--- RasPiLRobot.py
+++ RasPiLRobot.py
@@ -2,14 +2,8 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM) 
-# GPIO.setup(2,GPIO.OUT)
-# for iLoop in range(5):
-#    GPIO.output(2,True)   
-#    time.sleep(1)
-#    GPIO.output(2,False)   
-#    time.sleep(1)
 
-gp_out = 3
+gp_out = 29
 GPIO.setup(gp_out, GPIO.OUT)
 
 # motor = GPIO.PWM([channel],[frequency(Hz)])
