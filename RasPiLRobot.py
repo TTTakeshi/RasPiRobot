a70bfeb90a98cbeae34228d5612a2b510df599b0
剛史 瀧口 <Takeshi.Takiguchi0403@outlook.com>
2019-02-11 20:09:09 +0900

    thread

--- RasPiLRobot.py
+++ RasPiLRobot.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+import threading
 
 GPIO.setmode(GPIO.BCM) 
 
@@ -12,40 +13,58 @@ bot2 = 2.5   # -90 = 0.5ms / 20ms = 2.5%
 mid2 = 7.25   #   0 = 1.45ms / 20ms = 7.25%
 top2 = 12.0  # +90 = 2.4ms / 20ms = 12.0%
 
+class MyThread1(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.name = "MyThread1"    
 
-# right arm
-gp_out = 3
-GPIO.setup(gp_out, GPIO.OUT)
+     def run(self):
+        # right arm
+        gp_out = 3
+        GPIO.setup(gp_out, GPIO.OUT)
 
-motor = GPIO.PWM(gp_out, 50)
-motor.start(0.0)
+        motor = GPIO.PWM(gp_out, 50)
+        motor.start(0.0)
 
-motor.ChangeDutyCycle(bot)
-time.sleep(0.5)
+        motor.ChangeDutyCycle(bot)
+        time.sleep(0.5)
 
-motor.ChangeDutyCycle(top)
-time.sleep(0.5)
+        motor.ChangeDutyCycle(top)
+        time.sleep(0.5)
 
-motor.ChangeDutyCycle(mid)
-time.sleep(0.5)
+        motor.ChangeDutyCycle(mid)
+        time.sleep(0.5)
 
+class MyThread2(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.name = "MyThread2"
 
-# left arm
-gp_out2 = 21
-GPIO.setup(gp_out2, GPIO.OUT)
+     def run(self):
+        # left arm
+        gp_out2 = 21
+        GPIO.setup(gp_out2, GPIO.OUT)
 
-# motor = GPIO.PWM([channel],[frequency(Hz)])
-motor2 = GPIO.PWM(gp_out2, 50)
-motor2.start(0.0)
+        # motor = GPIO.PWM([channel],[frequency(Hz)])
+        motor2 = GPIO.PWM(gp_out2, 50)
+        motor2.start(0.0)
 
-motor2.ChangeDutyCycle(top2)
-time.sleep(0.5)
+        motor2.ChangeDutyCycle(top2)
+        time.sleep(0.5)
 
-motor2.ChangeDutyCycle(bot2)
-time.sleep(0.5)
+        motor2.ChangeDutyCycle(bot2)
+        time.sleep(0.5)
 
-motor2.ChangeDutyCycle(mid2)
-time.sleep(0.5)
+        motor2.ChangeDutyCycle(mid2)
+        time.sleep(0.5)
 
+thread1 = MyThread1()
+thread2 = MyThread2()
+
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
 
 GPIO.cleanup()
